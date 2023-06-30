9bf7bdf508a4d0bd1c43baa49138e28d62da2dc9
Ignas Burlingis <ignas.burlingis@gmail.com>
2023-06-30 09:06:27 +0300

    open/close animation example from angular docs

--- src/app/animations/animations.module.ts
+++ src/app/animations/animations.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnimationsComponent } from './animations.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { OpenCloseComponent } from './open-close/open-close.component';
 
 
 @NgModule({
   declarations: [
-    AnimationsComponent
+    AnimationsComponent,
+    OpenCloseComponent
   ],
   imports: [
     CommonModule,
     BrowserAnimationsModule
+  ],
+  exports: [
+    AnimationsComponent
   ]
 })
 export class AnimationsModule { }
---
+++ src/app/animations/open-close/open-close.component.ts
@@ -0,0 +1,36 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-open-close',
+  templateUrl: './open-close.component.html',
+  styleUrls: ['./open-close.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.8,
+        backgroundColor: 'blue'
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ])
+    ])
+  ]
+})
+export class OpenCloseComponent {
+  isOpen = true;
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+}

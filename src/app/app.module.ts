756b00521079faafee0cfbba46d40e8cdefbe87f
Ignas Burlingis <ignas.burlingis@gmail.com>
2023-07-05 22:25:18 +0300

    feat(navbar): refactored links to navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AnimationsModule } from './animations/animations.module';
+import { CoreModule } from './core/core.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AnimationsModule } from './animations/animations.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AnimationsModule
+    AnimationsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
